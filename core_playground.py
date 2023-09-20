d393de4c2fda5dab98a78dbd42d1608972727525
PiggyAwesome <48888771+PiggyAwesome@users.noreply.github.com>
2023-09-20 20:42:49 +0200

    Added basic proof of concept

---
+++ core_playground.py
@@ -0,0 +1,71 @@
+from sympy import symbols, solve, Eq, Equality, latex, pprint, init_printing
+init_printing(use_latex=True) 
+
+
+mass = symbols("m")
+time = symbols("t")
+acceleration = symbols("a")
+force = symbols("F")
+time = symbols("t")
+work = symbols("W")
+displacement = symbols("s")
+power = symbols("P")
+
+"""
+F = ma
+W = Fs
+P = W/∆t
+"""
+
+formulas:list[Equality] = [
+    Eq(force, mass*acceleration),
+    Eq(work, force*displacement),
+    Eq(power, work/time),
+]
+
+
+variables = {  
+    "F": force,
+    "a": acceleration,
+    "m": mass,
+    "W": work,
+    "s": displacement,
+    "t": time,
+    "P": power,
+}
+
+print("""
+      (F) Force
+      (a) Acceleration
+      (m) Mass
+      (W) Work
+      (s) Displacement
+      (t) Time
+      (P) Power
+...
+""")
+
+requested = input("What variable do you need?\n:: ")
+
+requested_object = variables.get(requested)
+
+possible_formulae = []
+for formula in formulas:
+    has_var = bool(formula.find(requested_object))
+    if has_var: possible_formulae.append(formula)
+
+
+for formula in possible_formulae:
+    required_variables:set = formula.free_symbols
+    required_variables.remove(requested_object)
+
+    for variable in required_variables:
+        has_variable = input(f"Do you have the value of ({variable})? (y/n): ")
+        if has_variable != "y": print("Checking next formula..."); break
+
+    else:
+
+        print(f"Your formula is the following:")
+        pprint(solve(formula, requested_object)[0], use_unicode=True)
+        break
+
